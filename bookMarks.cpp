99fb2f301e1a09d0ed15bdf7a00a3fc99df74fb0
looZ149 <nklitsch@web.de>
2025-12-16 16:28:41 +0100

    continued bookmarks stuff

--- bookMarks.cpp
+++ bookMarks.cpp
@@ -2,20 +2,41 @@
 #include <fstream>
 #include <filesystem>
 
-
-// void CBrowserBookMarks::SaveBookMark(std::string &url)
-// {
-//
-// }
-
-static void SafeBookMark(std::string &url) {
-    std::string path;
+static void SafeBookMark(const std::string &url) {
+    char buffer[MAX_PATH];
+    bool checkedDir = false;
 #if defined(_WIN32)
-    path = GetModuleFileNameA(NULL, path, MAX_PATH)
-    if (DirectoryExists)
+#include <windows.h>
+    GetModuleFileNameA(nullptr, buffer, MAX_PATH);
+
+    const std::filesystem::path exePath(buffer);
+    const std::filesystem::path exeDir = exePath.parent_path();
+    const std::string fileName = "Bookmarks.txt";
+
+    do
+    {
+        //Don't we need to check for the full path here with exePath, instead of exeDir?
+        if (std::filesystem::exists(exeDir) && std::filesystem::is_directory(exeDir))
+        {
+            //Only need to check if the dir exists, ofstream will create the file if it doesn't exist. Can we do the same for the dir?
+            std::ofstream file(fileName);
+            if (file.is_open())
+            {
+                file << url << std::endl;
+                file.close();
+                checkedDir = true;
+            }
+        }
+        else
+        {
+            std::filesystem::create_directory(exeDir);
+            //Jump back to start?
+        }
+    } while (!checkedDir);
+
 #endif
 #if defined(unix) || defined(__unix__) || defined(__unix)
-    path = std::filesystem::canonical("/proc/self/exe");
+    pathLen = std::filesystem::canonical("/proc/self/exe");
 #endif
 }
 
